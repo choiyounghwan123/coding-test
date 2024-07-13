f5ef7ea81a128480fa8aa4e185d517cbae402d72
choiyounghwan123 <fdgdfgdgf123@naver.com>
2024-07-13 14:05:21 +0900

    2630

---
+++ Class 3/2805.py
@@ -0,0 +1,26 @@
+# 나무 자르기
+
+import sys
+
+N,M = map(int,sys.stdin.readline().split())
+lengths = list(map(int,sys.stdin.readline().split()))
+
+start, end = 1,max(lengths)
+
+while start <= end:
+    mid = (start+end)//2
+    count = 0
+
+    for length in lengths:
+        if length-mid > 0:
+            count += length-mid
+    print(count,mid)
+
+    if N < count:
+        start = mid+1
+    else:
+        end = mid-1
+print(start)
+
+
+
